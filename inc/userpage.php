12401d78b7f6182b843f1ddaf99df28a7005c7d7
yozz <yozz.online@gmail.com>
2016-06-14 16:29:56 -0400

    Flushed deprecated

--- inc/userpage.php
+++ inc/userpage.php
@@ -11,8 +11,8 @@ add_filter( 'query_vars', 'userpage_rewrite_add_var' );
 function userpage_rewrite_rule() {
     add_rewrite_tag( '%member%', '([^&]+)' );
     add_rewrite_rule(
-        //'^member/([^/]*)/?',
-        '^([^/]*)/?',
+        '^member/([^/]*)/?',
+        //'^([^/]*)/?',
         'index.php?member=$matches[1]',
         'top'
     );
@@ -22,25 +22,29 @@ add_action('init','userpage_rewrite_rule');
 // Catch the URL and redirect it to a template file
 function userpage_rewrite_catch() {
     global $wp_query;
+    $user = get_user_by('login',$wp_query->query_vars['member']);
     
-    $user = get_userdatabylogin($wp_query->query_vars['member']);
-    
-
     if ( array_key_exists( 'member', $wp_query->query_vars ) && $user ) {
-        
-        
-        
-        function wpdev_before_after($content) {
-        
-        
-        global $wp_query;
-        $user = get_userdatabylogin($wp_query->query_vars['member']);   
-        $content = '[stickers user=' . $user->ID . ']';
-        $content = do_shortcode($content);
-        return $content;
+          
+        function userpage_content($content) {
+            global $wp_query;
+            $user = get_user_by('login',$wp_query->query_vars['member']);   
+            $content = '[stickers user=' . $user->ID . ']';
+            $content = do_shortcode($content);
+            return $content;
         }
-        add_filter('the_content', 'wpdev_before_after');
+        add_filter('the_content', 'userpage_content');
         
+//        function userpage_title($text) {
+//            global $wp_query;
+//            return $wp_query->query_vars['member'];
+//        }
+        //add_filter('the_title', 'userpage_title');
+        
+        function limit_1_post($limit) {
+            return 'LIMIT 1';
+        }
+        add_filter('post_limits', 'limit_1_post' );
         
         include (TEMPLATEPATH . '/page.php');
         exit;
